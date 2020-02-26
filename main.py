fcfe4dc99da506c6b3d4e6260d08979571f768f5
akira <>
2020-02-27 00:05:20 +0300

    "№№" слов нет

--- main.py
+++ main.py
@@ -3,28 +3,45 @@ import time
 import pprint
 import datetime
 import time
+import sys
 
-path =("c:\\Temp\\")
+
+
+
+path =("//Users//akira//Downloads//")
 scandiretories = os.listdir(path)
 time_item = os.stat(path).st_ctime
 time_item = time.ctime(int(time_item))
 times = []
+times.append(scandiretories)
+# my_string = ("__".join(times))
+# pprint.pprint(my_string)
 
-# for d in scandiretories():
-#     time_item = os.stat(d).st_ctime
+
+# d = os.stat(file).st_ctime
+
+for f in times:
+    k = len(f)
+    print(k)
+    # print(path + f)
+    # aas = os.stat(path + str(f)).st_ctime
+    # print(aas)
+    # time_item = os.stat(d).st_ctime
 #     time_item.append(times)
 # pprint.pprint(scandiretories)
+print(f)
 
-# print (time_item)
+# print (scandiretories)
+# print (scandiretories, time_item)
 
 
-for entry in os.scandir(path):
-    print((time.ctime(int(entry.stat().st_ctime))))
-    scandiretories = os.listdir(entry)
-    print (scandiretories)
+# for entry in os.scandir():
+#     print((time.ctime(int(entry.stat().st_ctime))))
+#     scandiretories = os.listdir(entry)
+#     print (scandiretories)
     # print(entry.path, entry.name, entry.stat())
 
-  
+
 
 # for filename in os.listdir(path):
 #     info = os.stat(filename)
@@ -41,8 +58,8 @@ for entry in os.scandir(path):
 
 # path = "/path/to/folder/"
 # Sort file names with path
-file_list = os.listdir(path)
-full_list = [os.path.join(path, i) for i in file_list]
-time_sorted_list = sorted(full_list, key = os.path.getmtime)
+# file_list = os.listdir(path)
+# full_list = [os.path.join(path, i) for i in file_list]
+# time_sorted_list = sorted(full_list, key = os.path.getmtime)
 
 # pprint.pprint(time_sorted_list)
