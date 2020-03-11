f4e7281e371e6694188ab7c329f206d6b7ca4cdc
akira <>
2020-03-11 23:31:25 +0300

    very many tuple выяснить почему ошибка на reduce

--- stackov.py
+++ stackov.py
@@ -18,46 +18,44 @@ def system():
         path = ("Home")
         return path
     if "Dar" in name_sys:
-        path = ("/Users/akira/Downloads/python и pyqt5 ирм/sudoku")
+        path = ("/Users/akira/Downloads/python и pyqt5 ирм/sudoku/modules")
         return path
 
-path = system()
-
-s = {}
-roots = {}
-di ={}
-files = {}
-
-w = os.walk(path)
-for data in w:
-
-    data = list(data)
-    # pprint.pprint(data)
-    # print(len(data))
-    a = data[0]
-    b = data[1]
-    v = data[2]
-
-    # b = list(b)
-    asep = a.split(os.sep)[-1]
-
-    # roots = dict.fromkeys(a)
-    folders = dict.fromkeys(b)
-    # print(folders)
-    files = dict.fromkeys(v)
-    # print(files)
-
-    s[asep] = folders,files
+# path = system()
 
 
+def get_structure():
+    path = system()
+    path = path.rstrip(os.sep)
+    start = path.rfind(os.sep) + 1
+    s = {}
+    w = os.walk(path, False)
+
+    for a, b, v in w:
+
+        # data = list(data)
+        # # pprint.pprint(data)
+        # # print(len(data))
+        # a = data[0]
+        # b = data[1]
+        # v = data[2]
+        asep = a[start:].split(os.sep)
+        subdir = dict.fromkeys(v)
+
+        print(asep)
+
+        folders = reduce(dict.get, asep[:-1], s)
 
 
+        folders[asep[-1]] = subdir
+    return s
+    pprint.pprint(s)
 
-    # folders = reduce(dict.get, b, roots)
+
+get_structure()
 
 
-
-pprint.pprint(s)
+#
 
 
 
