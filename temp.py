6a4b3a5f55ffc9d0bca518416413554c7d75220c
akira <>
2020-03-23 22:58:04 +0300

    получаю ключи в db_main. осталось записывать им значения

--- temp.py
+++ temp.py
@@ -1,16 +1,22 @@
 import datetime, time, data
 
-db = []
+db_main = []
 a = ['1','2','3','4','5']
-
+db_dict = {}
 dateNow = datetime.datetime.now()
-db.append(dateNow.strftime("%d-%m-%Y %H:%M"))
+db_main.append(dateNow.strftime("%d-%m-%Y %H:%M"))
 
 for names in a:
 	slovar = dict.fromkeys(names)
-	db.append(slovar)
+	db_dict.update(slovar)
+
+db_main.append(db_dict)
 
 
 
+print(db_main)
+db_key = db_main[1].keys()
+# добавляю значения по ключам
+for key in db_key:
+   print(key)
 
-print(db)
