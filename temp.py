bff112a94c8fd7fc0afbb4252b7b13a0ed665c9a
akira <>
2020-03-30 00:38:19 +0300

    убрал не нужное

--- temp.py
+++ temp.py
@@ -23,8 +23,7 @@ from_db_dict = ['asd', 'ssd', 'dsds', 'dsd', '2332']
 for keys, dates in db_dict.items():
     print(dates)
 
-# for bb in from_db_dict:
-# 	reduce(db_dict.update({key: bb}), from_db_dict)
+
 
 def rasschet(arg1, arg2):
 
@@ -39,13 +38,3 @@ for isd in puh:
 
 fol = reduce(rasschet, from_db_dict , db_dict)
 print(fol)
-
-# print(db_dict)
-
-
-
-# print(db_dict)
-# print(db_main[1])
-# from_dict = dict(db_main[1])
-# db_main[1] = from_db_dict
-# print(list(enumerate(from_db_dict)))
