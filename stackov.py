3e1175b101202b8b289d080c3bed834cb906e21f
linkomizin <linkomizin@gmail.com>
2020-04-01 13:21:44 +0300

    перевожу в функции. начинаю добавлять функционал сравнения

--- stackov.py
+++ stackov.py
@@ -31,8 +31,8 @@ def get_structure():
     start = path.rfind(os.sep) + 1
     dir = {}
     w = os.walk(path)
-    all_file = {}
-    fpath = []
+    db_all_file = {}
+    
     date_files = []
 
     for a, b, v in w:
@@ -40,6 +40,7 @@ def get_structure():
         asep = a[start:].split(os.sep)
 
         for files in v:
+            fpath = []
             fpath.append(os.path.join(a,files))
             pdata =(os.path.join(a,files))
             adate = (time.ctime(os.stat(pdata).st_ctime))
@@ -47,19 +48,20 @@ def get_structure():
 
 
             afile= dict(dict.fromkeys(fpath, adate))
-            all_file.update(afile)
+            db_all_file.update(afile)
 
         subdir = dict.fromkeys(v) # , os.stat(a.join(v)).st_ctime)
 
         folders = reduce(dict.get, asep[:-1], dir)
         folders[asep[-1]] = subdir
 
+    save_db_file(db_all_file)
 
 
-    fl = open("database.pcl", 'wb')
-    pickle.dump(dir, fl, 2)
-    pickle.dump(all_file, fl, 2)
-    fl.close()
+    # fl = open("database.pcl", 'wb')
+    # pickle.dump(dir, fl, 2)
+    # pickle.dump(db_all_file, fl, 2)
+    # fl.close()
     #print(fpath)
     #pprint.pprint(all_file)
 #    print('/__: ', len(all_file))
@@ -67,12 +69,30 @@ def get_structure():
 
     return dir
 
+def save_db_file(db_all_file):
+    fl = open("database.pcl", 'wb')
+    pickle.dump(db_all_file, fl, 2)
+    fl.close()
+
+
+
+
 
 
 
 get_structure()
-fl = open("database.pcl", 'rb')
-e1 = pickle.load(fl)
-e2 = pickle.load(fl)
-print(e2)
-fl.close()
+
+def load_db_file():
+    fl = open("database.pcl", 'rb')
+    e1 = pickle.load(fl)
+
+    
+    fl.close()
+    return (e1)
+
+
+def comparison_db():
+    e1 = load_db_file()
+    print(e1)
+
+comparison_db()
--- temp.py
+++ temp.py
@@ -5,6 +5,7 @@ db_main = []
 a = ['1', '2', '3', '4', '5']
 db_dict = {}
 dateNow = datetime.datetime.now()
+print(dateNow)
 db_main.append(dateNow.strftime("%d-%m-%Y %H:%M"))
 
 for names in a:
@@ -38,3 +39,22 @@ for isd in puh:
 
 fol = reduce(rasschet, from_db_dict , db_dict)
 print(fol)
+
+
+
+
+"""
+def dict_compare(d1, d2):
+    d1_keys = set(d1.keys())
+    d2_keys = set(d2.keys())
+    intersect_keys = d1_keys.intersection(d2_keys)
+    added = d1_keys - d2_keys
+    removed = d2_keys - d1_keys
+    modified = {o : (d1[o], d2[o]) for o in intersect_keys if d1[o] != d2[o]}
+    same = set(o for o in intersect_keys if d1[o] == d2[o])
+    return added, removed, modified, same
+
+x = dict(a=1, b=2)
+y = dict(a=2, b=2)
+added, removed, modified, same = dict_compare(x, y)
+"""
