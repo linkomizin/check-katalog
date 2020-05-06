83bbd18d5840fb2128d8869798a493739a364182
akira <>
2020-05-06 22:55:50 +0300

    Очистка ненужного подготовка к будущему слиянию на ветку master-dev

--- main.py
+++ main.py
@@ -1,128 +1,130 @@
 import os
 import time
 import pprint
-import datetime
-import time
+from datetime import datetime
+
 import sys
 import platform
 from functools import reduce
+import pickle
 
 print(platform.system())
 
 def system():
     name_sys = platform.system()
     if "Win" in name_sys:
-        path = ("C:\Temp\ПРиказы")
+        path = ("C:\\Temp\\ПРиказы")
         return path
     if "Lin" in name_sys:
-        path = ("Home")
+        path = (r"/storage/emulated/0/Documents")
         return path
     if "Dar" in name_sys:
-        path = ("//Users//akira//Downloads//python и pyqt5 ирм")
+        path = (r"/Users/akira/Downloads/python и pyqt5 ирм/sudoku")
         return path
 
 
-# path =
-path = system()
-scandiretories = os.listdir(path)
-time_item = os.stat(path).st_ctime
-time_item = time.ctime(int(time_item))
+def second_step():
+    if (os.path.exists('database.pcl')) == False:
+        get_structure(1)
+    elif(os.path.exists('database.pcl')) == True:
+        comparison_db()
 
-# my_string = ("__".join(times))
-# pprint.pprint(my_string)
+def get_structure(save_db):
+    path = system()
 
-# d = os.stat(file).st_ctime
+    path = path.rstrip(os.sep)
+    w = os.walk(path)
 
-w = os.walk(path, topdown=True)
-tst = {}
-catalogi = []
-directories = []
-files = []
-for cata, dir, file in w:
-    catalogi.append(cata.splitlines())
-    directories.append(dir)
-    files.append(file)
-    tst = dict.fromkeys(file)
-    print(dir)
+    db_all_file = {}
+    date_files = []
 
-# print(tst)
+    for a, b, v in w:
 
-# print(catalogi)
-# pprint.pprint(files)
+        for files in v:
+            fpath = []
+            fpath.append(os.path.join(a,files))
+            pdata =(os.path.join(a,files))
+            # adate = (time.ctime(os.stat(pdata).st_ctime))
+            adate = ((os.stat(pdata).st_ctime))
+            adate = datetime.fromtimestamp(adate).strftime("%d-%m-%Y %H:%M")
+            date_files.append(adate)
 
-# data_ct = {catalogi:}
-
+            afile= dict(dict.fromkeys(fpath, adate))
+            db_all_file.update(afile)
 
 
-
-    # data.update({"name_catalog": cata})
-#     for catalog_directory in cata:
-#         data.update({"name": catalog_directory})
-# #    print(data)
-#     a.append(data)
-# print(data)
+    if save_db == 1:
+        save_db_file(db_all_file)
+    elif save_db == 2:
+        return(db_all_file)
 
 
-def scaner1():
-    for f in scandiretories:
-
-        # print(f)
-        print(path + f)
-        aas = os.stat(path + f).st_ctime
-        aas = time.ctime(aas)
-        print(aas)
-
-
-def scaner2():
-    for entry in os.scandir(path):
-        d = entry.path
-        d = os.stat(d).st_ctime
-        d = time.ctime(d)
-        print(entry.path)
-        print(d)
-
-        # print(entry.path, entry.name, entry.stat())
+def save_db_file(db_all_file):
+    fl = open("database.pcl", 'wb')
+    pickle.dump(db_all_file, fl, 2)
+    fl.close()
 
 
 
+def load_db_file():
+    fl = open("database.pcl", 'rb')
+    e1 = pickle.load(fl)
+    fl.close()
+    return (dict(e1))
+
+def get_name_file(args):
+    if isinstance(args, set):
+        list=[]
+        for strting in args:
+            strting = str(strting)
+            file_name = strting.rsplit(os.sep, maxsplit=1)
+            list.append(file_name[1])
+        return list
+    elif isinstance(args, dict):
+        list=[]
+        for key in args:
+            print('!! yes ',key.rsplit(os.sep, maxsplit=1)[1])
 
 
 
-# for filename in os.listdir(path):
-#     info = os.stat(filename)
-#     pprint.pprint (info)
 
+def comparison_db():
+    e1 = load_db_file()
+    e2 = get_structure(2)
 
-# a = os.path.getatime(path)  # время последнего доступа
-# b = os.path.getmtime(path)  # время последнего изменения
-# c = os.path.getctime(path)  # время создания (Windows), время последнего изменения (Unix)
-# print([time.ctime(x) for x in [a, b, c]])
+    e1_keys = set(e1.keys())
+    e2_keys = set(e2.keys())
 
-# Необходимо отсортировать в коде файлы по дате создания, помог код:
+    removed_keys = e1_keys - e2_keys
+    if removed_keys:
+        get_name_file(removed_keys)
+        print('были удалены:\n', get_name_file(removed_keys), '\n')
+    added_keys = e2_keys - e1_keys
+    if added_keys:
+        get_name_file(added_keys)
+        print('были добавлены:\n', get_name_file(added_keys) )
 
+    print(len(added_keys), '|--| ', len(removed_keys) )
 
-# path = "/path/to/folder/"
-# Sort file names with path
-# file_list = os.listdir(path)
-# full_list = [os.path.join(path, i) for i in file_list]
-# time_sorted_list = sorted(full_list, key = os.path.getmtime)
+    # пересечения
+    intersect_keys = e1_keys.intersection(e2_keys)
+    if intersect_keys:
+        print('\nбыли найдены совпадения:\n', get_name_file(intersect_keys),'\n')
+    # print(('пересечения\n', intersect_keys))
 
-# pprint.pprint(time_sorted_list)
+    modififed = {o:(e1[o], e2[o]) for o in intersect_keys if e1[o] != e2[o] }
+    get_name_file(modififed)
+    # sravnenie(modififed)
+    # kl = (modififed.keys(), modififed.values())
+    # kl = (modififed.items())
+    # print(kl)
+    print(e2)
 
+def sravnenie(data):
 
+    for  keys, values in data:
+        print(values)
+        if values[1] > values[0]:
+            print('новое' )
 
-def get_directory_structure(rootdir):
-    """
-    Creates a nested dictionary that represents the folder structure of rootdir
-    """
-    dir = {}
-    rootdir = rootdir.rstrip(os.sep)
-    start = rootdir.rfind(os.sep) + 1
-    for path, dirs, files in os.walk(rootdir):
-        folders = path[start:].split(os.sep)
-        # print (folder)
-        subdir = dict.fromkeys(files)
-        parent = reduce(dict.get, folders[:-1], dir)
-        parent[folders[-1]] = subdir
-    return dir
-    print (dir)
+second_step()
